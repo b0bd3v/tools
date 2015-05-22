65a70925a81ed34a70bd26678e7923dfb993158d
Roberto Martins <roberto.martins.info@gmail.com>
2015-05-22 11:27:45 -0300

    Inciando desenvolvimento do shell script que irá automatizar minhas tarefas diárias

---
+++ run-tools.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##################################
+# General tools
+# Contact:
+# roberto.martins.info@gmail.com
+##################################
+
+numArgs="$#"
+echo "Number of args: $numArgs"
+
+
+taskKey="$1"
+
+echo "Mandando bala na terefa: $taskKey"
+#lastArg="${!#}"
+#echo "Last arg: $lastArg"
+
+
+
+
