024d7864918ecdb9affa6645fe9c8f314ebe5a92
Roberto Martins <roberto.martins.info@gmail.com>
2015-05-25 00:37:14 -0300

    Modificando nome do arquivo. Adicionando suporte a multiplos comandos

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##################################
+# General tools
+# Contact:
+# roberto.martins.info@gmail.com
+##################################
+
+numArgs="$#"
+#echo "Number of args: $numArgs"
+
+echo "Digite a chave de comando que deseja:"
+read taskKey
+
+case "$taskKey" in
+	"obj" ) 
+		echo "Começando os trabalhos na Object Edge"
+	;;
+	"hugweb")
+		echo "Começando os trabalho da Hugmedia Web"
+	;;	
+	"hugdroid")
+		echo "Começando os trabalho da Hugmedia Android"
+	;;
+esac
+
+#lastArg="${!#}"
+#echo "Last arg: $lastArg"
+
+
+
+
