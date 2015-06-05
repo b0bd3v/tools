12bb248329d726cb4b138a29131bbb27921446e5
Roberto Martins <roberto.martins.info@gmail.com>
2015-06-05 18:07:00 -0300

    Adicionando comando para abertura de dashboard moovweb

---
+++ scripts/obj-dashboard/obj-dashboard.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Abrindo Dashboard de tarefas"
+google-chrome "https://moovweb.atlassian.net/secure/Dashboard.jspa?selectPageId=19101"
+
--- scripts/obj/obj.sh
+++ scripts/obj/obj.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-source "./scripts/$taskKey/config.sh"
+source "$DIR/scripts/$taskKey/config.sh"
 echo "Inciando sdk Moovweb"
 gnome-terminal -e "sudo moov start"
-./run.sh gta
+$DIR/run gta
 #echo "Abrindo GTAgile"
 #google-chrome "http://gtagile.com.br/Priority/OE"
-echo "Abrindo Dashboard de tarefas"
-google-chrome "https://moovweb.atlassian.net/secure/Dashboard.jspa?selectPageId=19101"
+#echo "Abrindo Dashboard de tarefas"
+#google-chrome "https://moovweb.atlassian.net/secure/Dashboard.jspa?selectPageId=19101"
+$DIR/run obj-dashboard
+
 echo "Deseja abrir desenvolvimento em algum projeto? (Y/n)"
 read optionProjeto
 
