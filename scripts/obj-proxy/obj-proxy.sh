5292913f8b875e25bf460875bf92a724fe4f8eda
Roberto Martins <roberto.martins.info@gmail.com>
2015-06-10 10:11:27 -0300

    Adicionando funcionalidade de abrir ssh para o proxy em outro terminal

--- scripts/obj-proxy/obj-proxy.sh
+++ scripts/obj-proxy/obj-proxy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+source "$DIR/scripts/$taskKey/config.sh"
+
 export http_proxy="http://proxy.moov.sh:81"
 export https_proxy="http://proxy.moov.sh:81"
+
+#Abrir um terminal 
+$commandOpenSSH
+
 
