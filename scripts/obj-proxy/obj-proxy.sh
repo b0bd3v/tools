40dd7f510c4eaaf2f4b4d904addaefa79b033ffb
Roberto Martins <roberto.martins.info@gmail.com>
2015-06-12 17:39:23 -0300

    Melhorias proxy

--- scripts/obj-proxy/obj-proxy.sh
+++ scripts/obj-proxy/obj-proxy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 source "$DIR/scripts/$taskKey/config.sh"
 
+$commandOpenTerminal "$commandOpenSSH"
+
+
 export http_proxy="http://proxy.moov.sh:81"
 export https_proxy="http://proxy.moov.sh:81"
 
 #Abrir um terminal 
-$commandOpenTerminal "$commandOpenSSH"
 $commandOpenTerminal "sudo -E moov start"
